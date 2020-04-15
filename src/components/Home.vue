ce38308c1696a0603ce857b93f8e828afd9ce865
annhoang191 <hquynhanh19@gmail.com>
2020-04-15 21:12:18 +0700

    init

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <h2>
+
+      </h2>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data() {
+
+    }
+  }
+</script>
